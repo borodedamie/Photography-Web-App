f024031507bf245f2051cfd4c72d27f1faf6e039
unknown <opeoluborode@yahoo.com>
2019-04-22 23:49:23 +0100

    breadcrumbs home <-> blog

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -60,20 +60,22 @@
         </header>
 
         <!--================Home Banner Area =================-->
-        <section class="home_banner_area">
-           	<div class="box_1620">
-           		<div class="banner_inner d-flex align-items-center">
-					<div class="container">
-						<div class="banner_content text-center">
-							<h3>Nature <br />Photoshoot</h3>
-							<p>If you are looking at blank cassettes on the web, you may be very confused at the difference in price. You may see some for as low as $.17 each.</p>
-							<a class="main_btn" href="#">Explore Gallery</a>
-						</div>
-					</div>
-				</div>
-            </div>
-        </section>
-        <!--================End Home Banner Area =================-->
+        <section class="banner_area">
+                <div class="box_1620">
+                    <div class="banner_inner d-flex align-items-center">
+                        <div class="container">
+                            <div class="banner_content text-center">
+                                <h2>Blog</h2>
+                                <div class="page_link">
+                                    <a href="{{ route('home.index') }}">Home</a>
+                                    <a href="{{ route('home.blog') }}">Blog</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!--================End Home Banner Area =================-->
         <br>
         <br>
         <br>
